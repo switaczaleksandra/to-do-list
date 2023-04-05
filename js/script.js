6efa2b152ab1ffb45f5b85e144518e26c2f7a980
Aleksndra Świtacz <switaczaleksandra@gmail.com>
2023-04-05 18:24:03 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -10,6 +10,19 @@
         },
     ];
 
+    const addNewTask = (ewTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
 
@@ -18,20 +31,22 @@
         <li
         ${task.done ? " style=\"text-decoration: line-through\"" : ""}>
 
+        <button class="js-remove">usuń</button>
+
         ${task.content}
         </li>
         `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
 
-    const addNewTask = (ewTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
         });
-
-        render();
     };
 
     const onFormSubmit = (event) => {
