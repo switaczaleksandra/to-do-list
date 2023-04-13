01483ede53caaf3869879e599e8f84b186400129
Aleksndra Świtacz <switaczaleksandra@gmail.com>
2023-04-13 17:08:47 +0200

    immutability edit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,50 @@
 {
-    const tasks = [];
+    let tasks = [];
+    let hideDoneTasks = false;
 
     const removeTask = (taskIndex) => {
-        tasks.splice(taskIndex, 1);
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            ...tasks.slice(taskIndex + 1),
+        ];
+
         render();
     };
 
     const toggleTaskDone = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            {
+                ...tasks[taskIndex],
+                done: !tasks[taskIndex].done,
+            },
+            ...tasks.slice(taskIndex + 1),
+        ];
+
         render();
     };
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({ content: newTaskContent });
+        tasks = [
+            ...tasks,
+            { content: newTaskContent },
+        ];
+
+        render();
+    };
+
+    const toggleHideDoneTasks = () => {
+        hideDoneTasks = !hideDoneTasks;
+
+        render();
+    };
+
+    const markAllTasksDone = () => {
+        tasks = tasks.map((task) => ({
+            ...task,
+            done: true,
+        }));
+
         render();
     };
 
@@ -36,31 +68,69 @@
         });
     };
 
-    const render = () => {
-        let tasksListHTMLContent = "";
+    const renderTasks = () => {
+        const taskToHTML = task => `
+            <li class="
+                tasks__item${task.done && hideDoneTasks ? " tasks__item--hidden" : ""} js-tasks
+            ">
+                <button class="tasks__button tasks__button--toggleDone js-toggleDone">
+                    ${task.done ? "✓" : ""}
+                </button>
+                <span class="tasks__content${task.done ? " tasks__content--done" : ""}">
+                    ${task.content}
+                </span>
+                <button class="tasks__button tasks__button--remove js-remove">
+                🗑
+                </button>
+            </li >
+    `
+        const tasksElement = document.querySelector(".js-tasks");
+        tasksElement.innerHTML = tasks.map(taskToHTML).join("");
+    };
 
-        for (const task of tasks) {
-            tasksListHTMLContent += `
-        <li 
-        class="tasks__item js-task">
+    const renderButtons = () => {
+        const buttonsElement = document.querySelector(".js-buttons");
 
-        <button class="tasks__button tasks__button--toggleDone js-toggleDone">
-        ${task.done ? "✔️" : ""} 
-        </button>
-        
-        <span class="tasks__content${task.done ? " tasks__content--done" : ""}">${task.content}
-        </span>
-        <button class="tasks__button tasks__button--remove js-remove">
-        🗑️
-        </button>
-        </li>
-        `;
+        if (!tasks.length) {
+            buttonsElement.innerHTML = "";
+            return;
         };
 
-        document.querySelector(".js-tasks").innerHTML = tasksListHTMLContent;
 
+        buttonsElement.innerHTML = `
+        <button class="buttons__button js-toggleHideDoneTasks">
+            ${hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+        </button>
+        <button 
+            class="buttons__button js-markAllDone"
+            ${tasks.every(({ done }) => done) ? "disabled" : ""}
+        >
+            Ukończ wszystkie
+        </button>
+    `;
+    };
+
+    const bindButtonsEvents = () => {
+        const markAllDoneButton = document.querySelector(".js-markAllDone");
+
+        if (markAllDoneButton) {
+            markAllDoneButton.addEventListener("click", markAllTasksDone);
+        }
+
+        const toggleHideDoneTasksButton = document.querySelector(".js-toggleHideDoneTasks");
+
+        if (toggleHideDoneTasksButton) {
+            toggleHideDoneTasksButton.addEventListener("click", toggleHideDoneTasks);
+        }
+    };
+
+    const render = () => {
+        renderTasks();
         bindRemoveEvents();
         bindToggleDoneEvents();
+
+        renderButtons();
+        bindButtonsEvents();
     };
 
     const onFormSubmit = (event) => {
@@ -85,5 +155,4 @@
     };
 
     init();
-
 }
